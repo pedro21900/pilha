a465139078c98c591e92ab92e9b54e1f0aee511d
Pedro Lenonn Oliveira da Silva <pedro.lenonn@tre-pa.jus.br>
2021-07-09 11:06:01 -0300

    Correção bugs classe teste

--- src/test/java/topi/pilha/PilhaApplicationTests.java
+++ src/test/java/topi/pilha/PilhaApplicationTests.java
@@ -1,57 +1,58 @@
 package topi.pilha;
 
-import java.io.IOException;
-import java.util.List;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import br.com.erudio.models.Person;
-import br.com.erudio.repository.PersonRepository;
-import topi.pilha.model.Meal;
 import topi.pilha.model.MealItem;
-import topi.pilha.services.ThemealService;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 //Inicializamos o SpringRunner
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-class PilhaApplicationTests {
+@SpringBootTest
+
+public class PilhaApplicationTests {
     //URL base para onde as requests serão feitas
-    final String BASE_PATH = "http://localhost:8080";
-@Autowired
-    private ThemealService themealService;
+    final String BASE_PATH = "http://pilha-1625805102532.azurewebsites.net";
 
     //Definimos o restTemplate
     private RestTemplate restTemplate;
 
-    //Definimos o JacksonMapper responsável por converter
-    //JSON para Objeto e vice versa
     private ObjectMapper MAPPER = new ObjectMapper();
 
-    public void testFindOne() throws IOException{
+    @Test
+    public void testCreatePerson() throws IOException {
+        restTemplate = new RestTemplate();
 
-        //Fazemos uma requisição HTTP GET buscando por todas as pessoas
-        String response = restTemplate
-                .getForObject(BASE_PATH + "/", String.class);
+        HttpHeaders headers = new HttpHeaders();
 
-        //Pegamos o ID da pessoa na posição 1 da lista e fazemos nova requisição
-        //Recuperando as informações da mesma
-        Meal meal = themealService.searchMealById("52768");
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        HttpEntity entity = new HttpEntity(headers);
+
+        //Fazemos uma requisição HTTP GET buscando as meals
+        ResponseEntity<String> response = restTemplate
+                .exchange(BASE_PATH + "/api/meals?s=a", HttpMethod.GET, entity, String.class);
+
+        //Convertemos a resposta JSON para Objeto usando op Jackson
+
+        List<MealItem> meals = MAPPER
+                .readValue(response.getBody(), MAPPER.getTypeFactory()
+                        .constructCollectionType(List.class, MealItem.class));
 
         //Verificamos se o resultado da requisição é igual ao esperado
         //Se sim significa que tudo correu bem
-        Assert.assertNotNull(meal);
-        Assert.assertEquals("Apple Frangipan Tart", meal.getMeals().get(0).getStrMeal());
-        Assert.assertEquals("Silva", meal);
+        Assert.assertNotNull(meals.get(0));
+        Assert.assertEquals("Tart,Baking,Fruity", meals.get(0).getStrTags());
     }
 
 
