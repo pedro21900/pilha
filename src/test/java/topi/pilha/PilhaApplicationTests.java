943bc74e306ff25fcf367fecd4758ef6f6e8cb04
Pedro Lenonn Oliveira da Silva <pedro.lenonn@tre-pa.jus.br>
2021-07-09 11:08:11 -0300

    Correção imports não usados classe teste

--- src/test/java/topi/pilha/PilhaApplicationTests.java
+++ src/test/java/topi/pilha/PilhaApplicationTests.java
@@ -2,10 +2,8 @@ package topi.pilha;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
